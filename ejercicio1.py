89d5829ffe54108d4d0617db2404bc357940e361
sergioarevalog-uniminuto <sergio.arevalo-g@uniminuto.edu.co>
2026-02-27 21:53:06 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,19 @@
+#Este es un comentario
+#Uso de print para mostrar mensajes en pantalla
+#uso de input para solicitar datos al usuario
+print("Bienevenidos a la clase de Python")
+nombre=input("¿Cual es tu nombre? ")
+apellido=input("¿Cual es tu apellido? ")
+print("Hola",nombre,apellido,"bienvenid@ a la clase de Python")
+#Uso de variables para almacenar datos
+num1=45
+num2=23.5
+num3="Sergio Arevalo"
+print("Numero entero:",num1,"-","Numero decimal:",num2,"-","Cadena de texto:",num3)
+valor1=True
+valor2=False
+print("El dato que esta en la variable",valor1,"es de tipo", type(valor1))
+print("El dato que esta en la variable",valor2,"es de tipo", type(valor1))
+a,b,c=1,2,3
+print(a,b,c)
+print(a+b)
---
+++ promedionotas.py
@@ -0,0 +1,6 @@
+#Programa para sacar el promedio de notas
+nota1=int(input("¿Cual es la nota numero 1? "))
+nota2=int(input("¿Cual es la nota numero 2? "))
+nota3=int(input("¿Cual es la nota numero 3? "))
+promedio=(nota1+nota2+nota3)/3
+print("El promedio de las 3 notas es:",promedio)
