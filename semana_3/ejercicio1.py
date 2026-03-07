4e5a9422287017cf6d581ad8e8e0583b5c3cb14b
sergioarevalog-uniminuto <sergio.arevalo-g@uniminuto.edu.co>
2026-03-06 20:56:29 -0500

    Create ejercicio1.py

---
+++ semana_3/ejercicio1.py
@@ -0,0 +1,16 @@
+#Esto es un tipo de dato float
+a=7.5
+
+#Esto es un tipo de dato entero
+b=20
+print("El resultado de la suma es:", a+b)
+print("El resultado de la resta es:", a-b)
+print("El resultado de la multiplicacion es:", a*b)
+print("El resultado de la division es:", a/b)
+print("El resultado de la division con resultado entero es:", a//b) #Division con resultado entero
+print("El resultado del modulo es:", a%b) #Modulo de la division, es el residuo de la division
+
+#comparaciones
+print(a<b) #menor que
+print(a>b) #mayor que
+print(a==b) #Igual o igual
